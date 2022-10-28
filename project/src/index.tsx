a2dd7cb17a4cd479de239363580cbd6cde30d23d
oshagina6 <shaginao04@gmail.com>
2022-10-28 23:47:26 +0200

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_CARD_COUNT: 5,
+};
+
+//создаем root элемент
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
+//строгий режим для реакта
   <React.StrictMode>
-    <App />
+    <App
+      placeCardCount = {Setting.PLACE_CARD_COUNT}
+    />
   </React.StrictMode>,
 );
