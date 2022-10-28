3a1818d701eb9017d50590139b1c7c9e66159972
oshagina6 <shaginao04@gmail.com>
2022-10-28 23:55:21 +0200

    Начинаем программировать

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -79,7 +79,7 @@ function MainPage({ placeCardCount }: MainPageProps): JSX.Element {
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{placeCardCount} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-caption">Sort  by</span>
                 <span className="places__sorting-type" tabIndex={0}>
             Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
